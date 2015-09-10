701aa2999863a3070963e082d81ed6628638c6cd
joelsaldana <gwkogure@hotmail.com>
2015-09-10 15:16:34 -0600

    metodo pruebas
    metodo y mensaje de prueba

--- src/SSA/SeguroPopular.java
+++ src/SSA/SeguroPopular.java
@@ -2,8 +2,15 @@ package SSA;
 
 public class SeguroPopular {
 
+	public void pruebas(int a){
+		if (a==5) {
+			
+		}
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("GitHub Testing al 100%");
 
 	}
 
